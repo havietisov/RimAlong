3a10d6ab9bee52ddb6b24281b00844b000e1554e
gleb.avietisov <gleb.avietisov@aurumdust.com>
2018-09-03 18:01:39 +0300

    Initial POC All non-draft jobs are syncronized

---
+++ CooperateRim/DesignationManagerPatch.cs
@@ -0,0 +1,29 @@
+﻿using Harmony;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+
+namespace CooperateRim
+{
+    [HarmonyPatch(typeof(Verse.DesignationManager))]
+    [HarmonyPatch("AddDesignation")]
+    class DesignationManagerPatch
+    {
+        [HarmonyPrefix]
+        public static bool AddDesignation(ref Designation newDes)
+        {
+            if (!SyncTickData.IsDeserializing)
+            {
+                CooperateRimming.Log("AddDesignation++");
+                SyncTickData.AppendSyncTickData(newDes);
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
---
+++ CooperateRim/JobTrackerPatch.cs
@@ -0,0 +1,36 @@
+﻿using Harmony;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Text;
+using Verse;
+using Verse.AI;
+
+namespace CooperateRim
+{
+    //TryTakeOrderedJobPrioritizedWork
+    [HarmonyPatch(typeof(Verse.AI.Pawn_JobTracker))]
+    [HarmonyPatch("TryTakeOrderedJobPrioritizedWork")]
+    class JobTrackerPatch
+    {
+        [HarmonyPrefix]
+        public static bool TryTakeOrderedJobPrioritizedWork(Job job, WorkGiver giver, IntVec3 cell)
+        {
+            //job.def
+            CooperateRimming.Log("TryTakeOrderedJobPrioritizedWork.IsDes : " + SyncTickData.IsDeserializing);
+            if (!SyncTickData.IsDeserializing)
+            {
+                SyncTickData.AllowJobAt(job, giver, cell);
+                return false;
+                //cData.Enqueue(new CachedJobData() { job = job, tag = tag, __instance = __instance });
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
