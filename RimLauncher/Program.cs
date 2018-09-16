12618d85cb95adb093e48a3ee79f49731dd73416
gleb.avietisov <gleb.avietisov@aurumdust.com>
2018-09-17 01:08:03 +0300

    Building sync fixed - now they can be built of any material and in any orientation

--- RimLauncher/Program.cs
+++ RimLauncher/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,10 +12,15 @@ namespace RimLauncher
     {
         static Process p;
 
+        [DllImport("user32.dll")]
+        static extern int SetWindowText(IntPtr hWnd, string text);
+        
         static void Main(string[] args)
         {
             p = Process.Start(new ProcessStartInfo() { FileName = @"D:\SteamLibrary\steamapps\common\RimWorld\RimWorldWin64.exe", Arguments = "network_launch" });
             ChildProcessTracker.AddProcess(p);
+            System.Threading.Thread.Sleep(5000);
+            SetWindowText(p.MainWindowHandle, "Rimworld launched by 6opoDuJIo launcher to handle OBS mess!");
             p.WaitForExit();
             
         }
