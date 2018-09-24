d2faf379f47bde0f6d10cdf4ab2506418bd19085
gleb.avietisov <gleb.avietisov@aurumdust.com>
2018-09-24 23:43:24 +0300

    bills partially synced

---
+++ CooperateRim/WindowStackPatch.cs
@@ -0,0 +1,21 @@
+﻿using Harmony;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+
+namespace CooperateRim
+{
+    [HarmonyPatch(typeof(WindowStack))]
+    [HarmonyPatch("get_WindowsForcePause")]
+    public class WindowStackPatch
+    {
+        [HarmonyPrefix]
+        public static bool Prefix(ref bool __result)
+        {
+            __result = false;
+            return false;
+        }
+    }
+}
