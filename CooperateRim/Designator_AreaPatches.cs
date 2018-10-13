40027f5e2535bf5527b1aa01adaf4aa3780b12f6
borodilo <borodilo2@gmail.com>
2018-10-13 23:58:00 +0300

    Fixed building getting fucked up

--- CooperateRim/Designator_AreaPatches.cs
+++ CooperateRim/Designator_AreaPatches.cs
@@ -8,6 +8,41 @@ using Verse;
 
 namespace CooperateRim
 {
+    [HarmonyPatch(typeof(Designator_Build))]
+    [HarmonyPatch("DesignateSingleCell")]
+    public class Designator_build
+    {
+        [HarmonyPrefix]
+        public static bool Prefix(ref Designator __instance, ref IntVec3 c, ref BuildableDef ___entDef, ref Rot4 ___placingRot, ref ThingDef ___stuffDef)
+        {
+            CooperateRimming.Log("Designator_Build designate single cell " + (___stuffDef == null ? "null" : ___stuffDef.ToString()) + " || " + (___stuffDef == null ? "null" : ___stuffDef.defName));
+            ThingDef td = ___stuffDef;
+
+            if (!SyncTickData.AvoidLoop)
+            {
+                //DefDatabase<ThingDef>.AllDefs
+                SyncTickData.AppendSyncTickDataDesignatorSingleCell(__instance, c, ___entDef, ___placingRot, ___stuffDef);
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+            /*
+            CooperateRimming.Log("DesignateMultiCell_1");
+            if (!SyncTickData.AvoidLoop)
+            {
+                SyncTickData.AppendSyncTickData(__instance, cells);
+                return false;
+            }
+            else
+            {
+                return true;
+            }*/
+        }
+    }
+
+
     [HarmonyPatch(typeof(Designator_ZoneDelete))]
     [HarmonyPatch("DesignateSingleCell")]
     class Designator_patch__
@@ -26,7 +61,6 @@ namespace CooperateRim
                 return true;
             }
         }
-
     }
 
     //[HarmonyPatch(typeof(Designator_AreaBuildRoof))]
@@ -64,6 +98,10 @@ namespace CooperateRim
 
         public static bool DesignateMultiCell(ref Designator __instance, IEnumerable<IntVec3> cells)
         {
+            if (__instance is Designator_Build)
+            {
+                return true;
+            }
             CooperateRimming.Log("DesignateMultiCell_1");
             if (!SyncTickData.AvoidLoop)
             {
