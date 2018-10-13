936400339866bdbcbde0dd654dffb331b859b22a
gleb.avietisov <gleb.avietisov@aurumdust.com>
2018-10-13 19:22:58 +0300

    new designator patches. more generic

--- CooperateRim/Designator_AreaPatches.cs
+++ CooperateRim/Designator_AreaPatches.cs
@@ -10,15 +10,57 @@ namespace CooperateRim
 {
     //[HarmonyPatch(typeof(Designator_AreaBuildRoof))]
     //[HarmonyPatch("DesignateSingleCell")]
-    class Designator_AreaPatch
+    class Designator_patch
     {
         //[HarmonyPrefix]
-        public static bool DesignateSingleCell(ref Designator_Area __instance, ref IntVec3 c)
+        public static bool DesignateSingleCell_1(ref Designator __instance, ref IntVec3 c)
         {
             CooperateRimming.Log(__instance.GetType().AssemblyQualifiedName + ".DesignateSingleCell");
             if (!SyncTickData.AvoidLoop)
             {
-                SyncTickData.AppendSyncTickDataArea(__instance, c);
+                SyncTickData.AppendSyncTickData(__instance, c);
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public static bool DesignateSingleCell_2(ref Designator __instance, ref IntVec3 loc)
+        {
+            CooperateRimming.Log(__instance.GetType().AssemblyQualifiedName + ".DesignateSingleCell");
+            if (!SyncTickData.AvoidLoop)
+            {
+                SyncTickData.AppendSyncTickData(__instance, loc);
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public static bool DesignateMultiCell(ref Designator __instance, IEnumerable<IntVec3> cells)
+        {
+            CooperateRimming.Log("DesignateMultiCell_1");
+            if (!SyncTickData.AvoidLoop)
+            {
+                SyncTickData.AppendSyncTickData(__instance, cells);
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public static bool DesignateThing(Designator __instance, Thing t)
+        {
+            CooperateRimming.Log(__instance.GetType().AssemblyQualifiedName + ".DesignateThing");
+            if (!SyncTickData.AvoidLoop)
+            {
+                SyncTickData.AppendSyncTickDesignatorApplyToThing(__instance, t, t.Position);
                 return false;
             }
             else
