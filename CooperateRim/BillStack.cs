19cf85a3afc331489840281c5f8d4bc1c90f0016
borodilo <borodilo2@gmail.com>
2018-10-30 22:43:30 +0200

    bill removal/addition is finally done

--- CooperateRim/BillStack.cs
+++ CooperateRim/BillStack.cs
@@ -7,6 +7,74 @@ using System.Text;
 
 namespace CooperateRim
 {
+    [HarmonyPatch(typeof(BillStack))]
+    [HarmonyPatch("Delete")]
+    public class bill_delete_patch
+    {
+        static bool avoid_loop_internal = false;
+
+        public static void RemoveAt(BillStack stack, int index)
+        {
+            avoid_loop_internal = true;
+            try
+            {
+                stack.Delete(stack.Bills[index]);
+            }
+            finally
+            {
+                avoid_loop_internal = false;
+            }
+        }
+
+        [HarmonyPrefix]
+        public static bool Delete(ref Bill bill, BillStack __instance)
+        {
+            if (avoid_loop_internal)
+            {
+                return true;
+            }
+            else
+            {
+                RemoveAt(__instance, __instance.Bills.IndexOf(bill));
+                return false;
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(BillStack))]
+    [HarmonyPatch("Reorder")]
+    public class bill_reorder_patch
+    {
+        static bool avoid_loop_internal = false;
+
+        public static void ReorderAt(BillStack stack, int index, int offset)
+        {
+            avoid_loop_internal = true;
+            try
+            {
+                stack.Reorder(stack.Bills[index], offset);
+            }
+            finally
+            {
+                avoid_loop_internal = false;
+            }
+        }
+
+        [HarmonyPrefix]
+        public static bool Reorder(ref Bill bill, BillStack __instance, int offset)
+        {
+            if (avoid_loop_internal)
+            {
+                return true;
+            }
+            else
+            {
+                ReorderAt(__instance, __instance.Bills.IndexOf(bill), offset);
+                return false;
+            }
+        }
+    }
+
     /*
     [HarmonyPatch(typeof(BillStack))]
     [HarmonyPatch("AddBill")]
