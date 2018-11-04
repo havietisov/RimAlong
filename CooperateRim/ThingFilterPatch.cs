9d9e0dd534d719e2250138f255e1ab49ea577280
borodilo <borodilo2@gmail.com>
2018-11-04 03:02:34 +0200

    thingfilters now works!

---
+++ CooperateRim/ThingFilterPatch.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CooperateRim
+{
+    public class ThingFilterPatch
+    {
+        public static Stack<object> thingFilterCallerStack = new Stack<object>();
+    }
+}
---
+++ CooperateRim/UtilTable.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CooperateRim
+{
+    class UtilTable<T1, T2>
+    {
+        static Dictionary<T1, WeakReference> directTable = new Dictionary<T1, WeakReference>();
+        public static T2 TryFindFor(T1 obj)
+        {
+            WeakReference wr = directTable.ContainsKey(obj) ? directTable[obj] : null;
+            return wr != null && wr.IsAlive ? (T2)wr.Target : default(T2);
+        }
+
+        public static void Register(T1 obj, T2 keeper)
+        {
+            directTable.Add(obj, new WeakReference(keeper));
+        }
+    }
+}
