f153664852d41f4bed8168ef18155ebdffa4b626
borodilo <borodilo2@gmail.com>
2018-11-11 03:50:19 +0200

    recovered required stuff to continue contextualizing root tick calls

---
+++ CooperateRim/ContextualizedRand.cs
@@ -0,0 +1,22 @@
+﻿using Harmony;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Verse;
+using Verse.AI;
+
+namespace CooperateRim
+{
+    /*
+    [HarmonyPatch(typeof(ThinkNodePriority), "T")]
+    public class thinkTreeDefpatch
+    {
+        [HarmonyPrefix]
+        public static void prefix(ThinkNode rootNode,ref  int startHash)
+        {
+            startHash = 0;
+        }
+    }*/
+}
