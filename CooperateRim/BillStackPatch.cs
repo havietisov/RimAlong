d261f5c7a6a43065a11abaef5ba38fb5a0650f28
Gleb Avietisov <borodilo2@gmail.com>
2018-11-17 17:35:12 +0200

    Re-pushed to avoid destructive changes

---
+++ CooperateRim/BillStackPatch.cs
@@ -0,0 +1,32 @@
+﻿using Harmony;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+
+namespace CooperateRim
+{
+    [HarmonyPatch(typeof(BillStack), "DoListing")]
+    class BillStackPatch
+    {
+        [HarmonyPrefix]
+        public static void Prefix(ref Func<List<FloatMenuOption>> recipeOptionsMaker, ITab_Bills __instance)
+        {
+            Func<List<FloatMenuOption>> bill_source = recipeOptionsMaker;
+
+            recipeOptionsMaker = () =>
+            {
+                List<FloatMenuOption> optList = bill_source();
+
+                foreach (FloatMenuOption opt in optList)
+                {
+                    opt.action = () => { CooperateRimming.Log("recipe option clicked "); };
+                }
+
+                return optList;
+            };
+        }
+    }
+}
