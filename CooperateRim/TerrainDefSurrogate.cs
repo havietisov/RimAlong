75793810629f1fa3ef1846973de71694115f4868
borodilo <borodilo2@gmail.com>
2018-12-13 23:39:27 +0200

    Fixed TerrainDefSurrogate missing

---
+++ CooperateRim/TerrainDefSurrogate.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.Serialization;
+using Verse;
+using System.Linq;
+
+namespace CooperateRim
+{
+    public class TerrainDefSurrogate : ISerializationSurrogate
+    {
+        public void GetObjectData(object obj, SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("defname", ((TerrainDef)obj).defName);
+        }
+
+        public object SetObjectData(object obj, SerializationInfo info, StreamingContext context, ISurrogateSelector selector)
+        {
+            string s = info.GetString("defname");
+            return DefDatabase<JobDef>.AllDefs.First(u=> u.defName == s);
+        }
+    }
+}
