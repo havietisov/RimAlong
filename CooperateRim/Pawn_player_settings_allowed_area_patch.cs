c5740535549114ca86a6bcc4936ff0188aa41046
borodilo <borodilo2@gmail.com>
2018-12-13 20:26:02 +0200

    Moved area restriction patch to it's own file

---
+++ CooperateRim/Pawn_player_settings_allowed_area_patch.cs
@@ -0,0 +1,34 @@
+﻿using Verse;
+using RimWorld;
+using Harmony;
+
+[HarmonyPatch(typeof(Pawn_PlayerSettings), "set_AreaRestriction")]
+public class Pawn_player_settings_allowed_area_patch : common_patch_fields
+{
+    public static void set_restriction(Pawn pawn, Area area)
+    {
+        use_native = true;
+        try
+        {
+            pawn.playerSettings.AreaRestriction = area;
+        }
+        finally
+        {
+            use_native = false;
+        }
+    }
+
+    [HarmonyPrefix]
+    public static bool prefix(Pawn ___pawn, Area value)
+    {
+        if (use_native)
+        {
+            return true;
+        }
+        else
+        {
+            set_restriction(___pawn, value);
+            return false;
+        }
+    }
+}
--- CooperateRim/designator_claim_methods.cs
+++ CooperateRim/designator_claim_methods.cs
@@ -2,38 +2,6 @@
 using RimWorld;
 using System.Runtime.Serialization;
 using System.Linq;
-using Harmony;
-
-[HarmonyPatch(typeof(Pawn_PlayerSettings), "set_AreaRestriction")]
-public class Pawn_player_settings_allowed_area_patch : common_patch_fields
-{
-    public static void set_restriction(Pawn pawn, Area area)
-    {
-        use_native = true;
-        try
-        {
-            pawn.playerSettings.AreaRestriction = area;
-        }
-        finally
-        {
-            use_native = false;
-        }
-    }
-
-    [HarmonyPrefix]
-    public static bool prefix(Pawn ___pawn, Area value)
-    {
-        if (use_native)
-        {
-            return true;
-        }
-        else
-        {
-            set_restriction(___pawn, value);
-            return false;
-        }
-    }
-}
 
 public class designator_claim_methods : common_patch_fields
 {
